cd3dcf7588c8a3f1647a10fedbc19708e4654b59
student-vedansh <35373105+ebe25@users.noreply.github.com>
2024-03-19 00:54:22 +0530

    chore:daaisyUI configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,157 @@
-import { useState } from 'react'
-import { SignupFormDemo } from './components/SignupForm'
-
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <SignupFormDemo/>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="flex  h-screen">
+        <ul className="menu bg-base-200 lg:menu-horizontal rounded-box">
+          <li>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              Inbox
+              <span className="badge badge-sm">99+</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Updates
+              <span className="badge badge-sm badge-warning">NEW</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              Stats
+              <span className="badge badge-xs badge-info"></span>
+            </a>
+          </li>
+        </ul>
+        <div class="h-screen flex items-center justify-evenly">
+        <button class="btn btn-accent btn-outline">Button</button>
+        <div className="card w-96 glass">
+          <figure>
+            <img
+              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="car!"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-bubble chat-bubble-primary">
+            What kind of nonsense is this
+          </div>
+        </div>
+      </div>
+      </div>
+     
+      <div>
+        <div className="chat chat-start">
+          <div className="chat-bubble chat-bubble-secondary">
+            Put me on the Council and not make me a Master!??
+          </div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-bubble chat-bubble-accent">
+            That's never been done in the history of the Jedi. It's insulting!
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble chat-bubble-success">
+            You have been given a great honor.
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble chat-bubble-warning">
+            To be on the Council at your age.
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble chat-bubble-error">
+            It's never happened before.
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
